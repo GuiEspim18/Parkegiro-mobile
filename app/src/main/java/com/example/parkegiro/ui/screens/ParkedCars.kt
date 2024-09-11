811710816ef822c8805dceaa20f91c0ec52272de
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-09-11 09:25:59 -0300

    parked cars

--- app/src/main/java/com/example/parkegiro/ui/screens/ParkedCars.kt
+++ app/src/main/java/com/example/parkegiro/ui/screens/ParkedCars.kt
@@ -1,15 +1,60 @@
 package com.example.parkegiro.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.parkegiro.R
+import com.example.parkegiro.ui.components.cards.DoubleTextCard
 import com.example.parkegiro.ui.components.contentHolder.ScrollContentHolder
 
 @Composable
 fun ParkedCars() {
     Column {
         ScrollContentHolder(title = "Carros Estacionados", icon = R.drawable.car, divider = false) {
+            Spacer(modifier = Modifier.height(20.dp))
+            DoubleTextCard(
+                firstText = "Peugeot 308 THP - Branco",
+                secondText = "Placa: GBR-9F90  Entrada: 08:30"
+            )
 
+            Spacer(modifier = Modifier.height(20.dp))
+            DoubleTextCard(
+                firstText = "Peugeot 308 THP - Branco",
+                secondText = "Placa: GBR-9F90  Entrada: 08:30"
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+            DoubleTextCard(
+                firstText = "Peugeot 308 THP - Branco",
+                secondText = "Placa: GBR-9F90  Entrada: 08:30"
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+            DoubleTextCard(
+                firstText = "Peugeot 308 THP - Branco",
+                secondText = "Placa: GBR-9F90  Entrada: 08:30"
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+            DoubleTextCard(
+                firstText = "Peugeot 308 THP - Branco",
+                secondText = "Placa: GBR-9F90  Entrada: 08:30"
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+            DoubleTextCard(
+                firstText = "Peugeot 308 THP - Branco",
+                secondText = "Placa: GBR-9F90  Entrada: 08:30"
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+            DoubleTextCard(
+                firstText = "Peugeot 308 THP - Branco",
+                secondText = "Placa: GBR-9F90  Entrada: 08:30"
+            )
         }
     }
 }
