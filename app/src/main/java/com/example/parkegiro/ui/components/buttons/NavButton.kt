22cc23112382a0889b843f545852355300f038ed
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-09-12 21:45:05 -0300

    nav button selected

--- app/src/main/java/com/example/parkegiro/ui/components/buttons/NavButton.kt
+++ app/src/main/java/com/example/parkegiro/ui/components/buttons/NavButton.kt
@@ -1,12 +1,28 @@
 package com.example.parkegiro.ui.components.buttons
 
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.parkegiro.ui.theme.Gray
+import com.example.parkegiro.ui.theme.MainBlue
 
 @Composable
-fun NavButton(text: String, onClick: () -> Unit) {
-    Button(onClick = onClick) {
-        Text(text = text)
+fun NavButton(text: String, onClick: () -> Unit, selected: Boolean = false) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = if (!selected) Gray else MainBlue
+        ),
+        shape = RoundedCornerShape(topStart = 5.dp, topEnd = 5.dp, bottomEnd = 0.dp, bottomStart = 0.dp)
+    ) {
+        Text(
+            text = text,
+            color = if (!selected) MainBlue else Gray
+        )
     }
 }
