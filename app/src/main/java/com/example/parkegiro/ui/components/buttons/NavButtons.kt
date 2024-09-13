f0851905c974f75b15c0990e56147bcfb3c875cb
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-09-12 21:17:31 -0300

    buttons

--- app/src/main/java/com/example/parkegiro/ui/components/buttons/NavButtons.kt
+++ app/src/main/java/com/example/parkegiro/ui/components/buttons/NavButtons.kt
@@ -1,8 +1,9 @@
 package com.example.parkegiro.ui.components.buttons
 
 import androidx.compose.runtime.Composable
+import com.example.parkegiro.utils.interfaces.Button
 
 @Composable
-fun NavButtons() {
+fun NavButtons(buttons: List<Button>) {
 
 }
---
+++ app/src/main/java/com/example/parkegiro/utils/interfaces/Button.kt
@@ -0,0 +1,6 @@
+package com.example.parkegiro.utils.interfaces
+
+interface Button {
+    var text: String
+    var onClick: () -> Unit
+}
