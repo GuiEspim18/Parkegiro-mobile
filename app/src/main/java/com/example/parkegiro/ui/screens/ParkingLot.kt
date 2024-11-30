5d40bf15fc5969421f6eb7a8991c0e2291117079
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-11-30 14:22:17 -0300

    remove some imports

--- app/src/main/java/com/example/parkegiro/ui/screens/ParkingLot.kt
+++ app/src/main/java/com/example/parkegiro/ui/screens/ParkingLot.kt
@@ -1,23 +1,15 @@
 package com.example.parkegiro.ui.screens
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.parkegiro.R
-import com.example.parkegiro.ui.components.buttons.IconButton
-import com.example.parkegiro.ui.components.cards.DoubleTextCard
 import com.example.parkegiro.ui.components.contentHolder.ScrollContentHolder
 import com.example.parkegiro.ui.components.entrances.Entrances
 import com.example.parkegiro.ui.components.exits.Exits
 import com.example.parkegiro.ui.components.tabs.Tabs
-import com.example.parkegiro.ui.theme.Orange
-import com.example.parkegiro.ui.theme.Red
 import com.example.parkegiro.utils.interfaces.Tab
 
 @Preview(showBackground = true)
