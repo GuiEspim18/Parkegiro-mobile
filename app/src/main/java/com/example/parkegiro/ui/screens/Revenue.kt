156ea389e1821f430ddeee48b07292df50e013b8
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-12-01 11:56:33 -0300

    spacing

--- app/src/main/java/com/example/parkegiro/ui/screens/Revenue.kt
+++ app/src/main/java/com/example/parkegiro/ui/screens/Revenue.kt
@@ -1,7 +1,11 @@
 package com.example.parkegiro.ui.screens
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.parkegiro.R
 import com.example.parkegiro.ui.components.cards.DoubleTextCard
 import com.example.parkegiro.ui.components.contentHolder.ScrollContentHolder
@@ -16,12 +20,14 @@ fun Revenue() {
             icon = R.drawable.money,
             divider = false
         ) {
-            DoubleTextCard(
-                firstText = "GBR-9F90",
-                secondText = "Entrada: 08:00 - Saída: 18:00",
-                color = MainBlue,
-                textColor = White
-            )
+            Box(modifier = Modifier.padding(top = 10.dp, start = 15.dp, end = 15.dp)) {
+                DoubleTextCard(
+                    firstText = "GBR-9F90",
+                    secondText = "Entrada: 08:00 - Saída: 18:00",
+                    color = MainBlue,
+                    textColor = White
+                )
+            }
         }
     }
 }
