e9795082ba3d3d7d202bdf808a808fede8f6d505
GuiEspim18 <guilherme.monteiro.espim@gmail.com>
2025-04-15 08:51:07 -0300

    to react native

--- app/src/main/java/com/example/parkegiro/utils/interfaces/Button.kt
+++
@@ -1,3 +0,0 @@
-package com.example.parkegiro.utils.interfaces
-
-data class Button(val text: String, val onClick: () -> Unit)
--- app/src/main/java/com/example/parkegiro/utils/interfaces/Tab.kt
+++
@@ -1,5 +0,0 @@
-package com.example.parkegiro.utils.interfaces
-
-import androidx.compose.runtime.Composable
-
-data class Tab(val name: String, val content: @Composable () -> Unit)
